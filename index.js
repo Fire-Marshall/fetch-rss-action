b77176e0ba75ab67af180b82b3a33578d3246b30
wukaisen <kaisenwu@outlook.com>
2022-03-17 16:15:43 +0800

    Update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const exec = require('@actions/exec');
 const axios = require('axios');
 
 async function fetchGamersky() {
@@ -17,7 +18,40 @@ async function fetchGamersky() {
       },
     },
   });
-  console.log(res);
+  const { errorCode, errorMessage, result } = res.data;
+
+  const entries = result
+    .map(item => {
+      const link =
+        item.contentType === 'URL'
+          ? item.contentURL.replace(/\?.+/, '')
+          : `https://wap.gamersky.com/news/Content-${item.contentId}.html`;
+      return `
+  <entry>
+    <title>${item.title}</title>
+    <link href="${link}" rel="alternate"/>
+    <id>${link}</id>
+    <updated>${new Date(item.updateTime)}</updated>
+    <author><name>${item.authorName}</name></author>
+    <summary type="html">
+      <![CDATA[<img src="${item.thumbnailURLs[0]}" />]]>
+    </summary>
+  </entry>`;
+    })
+    .join('')
+    .trim();
+
+  const feed = `
+<?xml version="1.0" encoding="UTF-8"?>
+<feed xmlns="http://www.w3.org/2005/Atom">
+  <title>游民星空</title>
+  <link href="https://www.gamersky.com/" rel="alternate"/>
+  <updated>${new Date().toISOString()}</updated>
+  <id>https://www.gamersky.com/</id>
+  ${entries}
+</feed>
+`;
+  exec.exec(`echo -e ${feed} > rss.xml`);
 }
 
 try {
